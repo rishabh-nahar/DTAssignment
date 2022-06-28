3c2912bd5ed48c1826f9177bf0e19d68b9333cf1
Rishabh <90189129+rishabh-nahar@users.noreply.github.com>
2022-06-29 01:31:29 +0530

    Assignment finished

--- myScripts.js
+++ myScripts.js
@@ -1,8 +1,10 @@
-// console.log("hello")
+// variable storing assets in array
 let array_assets ;
+
+// fetching info from api (using local json file because of CORS problem)
 fetch("./info.json",{
     mode:'cors',
-    credentials: 'include', // include, *same-origin, omit
+    credentials: 'include',
     headers: {
       'Content-Type': 'application/json',
       'Access-Control-Allow-Origin':'http://127.0.0.1:5500'
@@ -16,6 +18,7 @@ fetch("./info.json",{
   )
 .catch(err => console.log(err))
 
+// function to read tasks and assets
 let jsonFunc = (data) => {
   task_title = data['tasks'][0].task_title // demo task
   document.getElementById("task_title").textContent = task_title
@@ -23,10 +26,13 @@ let jsonFunc = (data) => {
   array_assets = data['tasks'][0].assets // array of all assets
 
 
-
+  //calling the function to create dynamic elements
   createContainer(array_assets);
 }
 
+// predefined functions to create elements
+
+// fucntion to create text box
 function createTbWoBg(){
   let input_wo_bg = document.createElement("div");
   let input_wo_bg_textarea = document.createElement("textarea");
@@ -38,25 +44,8 @@ function createTbWoBg(){
   return input_wo_bg;
 }
 
-function createTbWBg(){
-  
-  let input_w_bg = document.createElement("div");
-  let input_w_bg_textarea = document.createElement("textarea");
-  let input_w_bg_title = document.createElement("div")
-  let input_w_bg_small_inp = document.createElement("div")
 
-  input_w_bg.classList.add("input-bg");
-  input_w_bg_title.classList.add('title');
-  input_w_bg_title.textContent= "Input Head"
-  input_w_bg_small_inp.classList.add('task-small-inp')
-
-  input_w_bg.appendChild(input_w_bg_title);
-  input_w_bg_small_inp.appendChild(input_w_bg_textarea);
-  input_w_bg.appendChild(input_w_bg_small_inp);
-
-  return input_w_bg;
-}
-
+// fucntion to create image element
 function createImg(img_src) {
 
   let image_div = document.createElement("div");
@@ -69,7 +58,7 @@ function createImg(img_src) {
 
 }
 
-
+// fucntion to create doc image and audio element
 
 function createDocs(doc_src){
   let doc_div = document.createElement("div");
@@ -81,6 +70,7 @@ function createDocs(doc_src){
   return doc_div
 }
 
+// fucntion to create para
 
 function createPara(para_content){
   let para = document.createElement("div")
@@ -88,6 +78,7 @@ function createPara(para_content){
   return para
 }
 
+// function to create collapsable description
 function createDescription(description,id){
   let desc_div = document.createElement("div")
   desc_div.textContent = description;
@@ -98,41 +89,7 @@ function createDescription(description,id){
   return desc_div
 }
 
-
-function option_container(option_values){
-
-    let option_container = document.createElement("div");
-    option_container.classList.add("option-container");
-    let select_box = document.createElement("select");
-    option_container.appendChild(select_box)
-
-
-    option_values.map((option_val)=>{
-        let option = document.createElement("option")
-        option.value = option_val;
-        option.textContent = option_val;
-        select_box.appendChild(option)
-
-      },this)
-
-
-    return option_container
-
-}
-
-
-
-function createButton(){
-    let blue_bttn = document.createElement("div")
-    let bttn = document.createElement("button");
-    blue_bttn.classList.add("blue-bttn");
-    blue_bttn.appendChild(bttn)
-    bttn.textContent = "Submit"
-    bttn.classList.add('bttn');
-  
-    return blue_bttn
-  }
-  
+// function to create angle arrow to show/hide collapsable description
 
 
 function createDownArrow(asset_data){
@@ -141,8 +98,8 @@ function createDownArrow(asset_data){
   let down_arrow_div = document.createElement("div")
   down_arrow_div.classList.add("down-arrow")
   down_arrow = document.createElement("i");
-  down_arrow.classList.add("fa") 
-  down_arrow.classList.add("fa-angle-down") 
+  down_arrow.classList.add("fa")
+  down_arrow.classList.add("fa-angle-down")
 
   let asset_unique_id = asset_data.asset_id;
 
@@ -156,13 +113,16 @@ function createDownArrow(asset_data){
   return down_arrow_div
 }
 
+
+// function to show/hide collapsable description
+
 function toggle_display(asset_unique_id,arrow){
   document.getElementById(asset_unique_id).classList.toggle('hide-show');
   arrow.classList.toggle('fa-angle-down')
   arrow.classList.toggle('fa-angle-up')
 }
 
-
+// function to make first letter capital
 function capitalizeFirstLetter(given_string){
   const str = given_string;
   const str2 = str.charAt(0).toUpperCase() + str.slice(1);
@@ -171,15 +131,16 @@ function capitalizeFirstLetter(given_string){
   return str2
 }
 
-
+//function to create dynamic elements based on given data
 
 let createContainer = (array_assets) => {
 
   task_container_group = document.querySelector('.task-container-group');
 
-  
+  // loop for traversing through each assets and creating element accordingly 
+
   for(let asset_data of array_assets){
-    
+
 
       let task_container_wrapper = document.createElement("div");
       let task_container = document.createElement("div");
@@ -192,6 +153,8 @@ let createContainer = (array_assets) => {
       heading.classList.add("heading")
       content.classList.add('content')
 
+      // above code has divided the task container into 2 parts heading and content 
+
       heading.textContent = capitalizeFirstLetter(asset_data.asset_title);
 
       task_container_group.appendChild(task_container_wrapper);
@@ -199,9 +162,8 @@ let createContainer = (array_assets) => {
       task_container.appendChild(heading);
       task_container.appendChild(content);
 
-
+      // code for what content to be shown
       if(asset_data.asset_type == "display_asset" && asset_data.asset_content == "reflection"){
-        // console.log("createPara()=>",asset_data.display_asset_reflection)
         content.appendChild(createPara(asset_data.display_asset_reflection))
       }
       else if(asset_data.asset_type == "input_asset" && asset_data.asset_content == "tb"){
@@ -233,43 +195,9 @@ let createContainer = (array_assets) => {
         task_container_group.removeChild(task_container_wrapper)
       }
 
+      // appending collapsable description
       content.appendChild(createDescription(asset_data.asset_description,asset_data.asset_id))
       content.appendChild(createDownArrow(asset_data))
 
   }
-}
-
-function createList(list_type,list_content){
-  list_container = document.createElement("div");
-  index = 0;
-  for(li__type of list_type){
-    
-    let li_type  = document.createElement(li__type);
-
-    for(li_s of list_content[index]){
-      let li = document.createElement("li");
-      li.textContent = li_s
-      li_type.appendChild(li)
-    }
-    index++;
-    list_container.appendChild(li_type);
-  }
-  return list_container;
-}
-
-
-
-function journeyBoard(){
-
-  journey_board_task_list = document.getElementById("task_lists")
-  journey_board_task_list.appendChild(createList(["ol","ul"],[["demo task"],['task1','task2','task3']]))
-
-}
-// journeyBoard();
-
-
-function toggleJourneyBoard(){
-  document.querySelector(".journey-board").classList.toggle('collapsed-journey-board');
-  document.querySelector(".task-lists").classList.toggle('hide-show')
-  document.querySelector(".collapsed-task").classList.toggle('show-hide')
 }
